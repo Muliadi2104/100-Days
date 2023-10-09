f88dd34a9ae168a8af0388092a7d454cc86c678a
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-09 21:40:53 +0800

    Hari Pertama

---
+++ 100 HariBelajarJava.java
@@ -0,0 +1,16 @@
+package belajarjava;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String nama = "muliadi";
+        char jeniskelamin = 'L';
+        int umur = 19;
+        
+        System.out.println(nama);
+        System.out.println(jeniskelamin);
+        System.out.println(umur);
+    }
+    
+}
