c715e4ca47575acc76bb244c60e5619c24935622
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-10 18:04:01 +0800

    Add files via upload

---
+++ coding 100 day2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     
+     String nama ="muliadi";
+    System.out.println("Nama saya \t\t: "+nama);
+        String alamat = "polman";
+        System.out.println("Alamat saya ada di \t: "+alamat);
+        double tinggi =17.0;
+        System.out.println("Tinggi badan saya\t: "+tinggi);
+    char kelas ='E';    
+        System.out.println("Saya dari Informatika\t: "+kelas);
+     
+     
+     
+     
+    
+   }
+ }
