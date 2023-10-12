c0cd59c43bc15974dc2f8e7b64d329b03e417923
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-12 09:44:13 +0800

    Create Day 3.java

---
+++ Day 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int a = 8;
+    int b = 12;
+    int hasil;
+    hasil= a-b;
+    System.out.println(hasil);
+   }
+ }
