2b933da2c7767cef484521d64f2cdbaca8e35fce
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-12 09:39:05 +0800

    Day 4

---
+++ Coding day 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int a = 7;
+    int b = 9;
+    int c;
+    c = a+b;
+    System.out.println(c);
+    
+   }
+ }
