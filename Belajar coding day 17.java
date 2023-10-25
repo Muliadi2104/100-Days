e52c28662e290873e43daeb119528eaaaedf1211
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-25 21:21:12 +0800

    Create Belajar coding day 17.java

---
+++ Belajar coding day 17.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     int nilai = 20;
+     nilai ++;
+     nilai+=5;
+     nilai-=2;
+     System.out.println (nilai);
+     
+     
+    
+   }
+ }
