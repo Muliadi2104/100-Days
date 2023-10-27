52358c5acd61fbfd6bc23192aff79b456731318d
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-27 17:45:49 +0800

    Create Belajar coding day 19.java

---
+++ Belajar coding day 19.java
@@ -0,0 +1,18 @@
+
+	import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int username = 2234;
+    int password = 1234;
+    
+    if (username == 1234 && password == 1234)
+      System.out.println ("LOGIN");
+      
+      else 
+      System.out.println ("GAGAL LOGIN");
+   }
+ }	
+	
