7089cbef9d6469de24b4c3e7d468682e95071aff
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-10-30 22:23:03 +0800

    Create BelajarCodingDay22.java

---
+++ BelajarCodingDay22.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int a = 20, b = 8;
+    
+    int jumlah = a+b;
+    int kurang = a-b;
+    
+    System.out.println ("penjumlahan :"+jumlah);
+    System.out.println ("pengurangan :"+ kurang);
+   
+   }
+ }
