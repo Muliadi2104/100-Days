93686ee7a4dc2aeca50802df2de9f3036945ecd8
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-01 22:55:15 +0800

    Update Belajar coding day 24.java

--- Belajar coding day 24.java
+++ Belajar coding day 24.java
@@ -1,17 +1,14 @@
-import java.util.*;
+package aaa;
 
-
- class Dcoder
- {
-   public static void main(String args[])
-   { 
-    System.out.println("Hello, Dcoder!");
-    for(;;){
-      System.out.println("hello-helle");
-      
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Perulangan Hitungan Terbalik. Dari angka 5-1.
+    for(int n = 5; n > 0; n--){
+      System.out.println("Urutan n ke-"+n);
       }
-     
-     
-      
-   }
- }
+        
+    }
+    
+}
