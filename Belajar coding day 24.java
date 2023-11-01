fc717a538d8437a99c43c4f81c3e77f095328f5c
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-01 19:38:48 +0800

    Create Belajar coding day 24.java

---
+++ Belajar coding day 24.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Dcoder!");
+    for(;;){
+      System.out.println("hello-helle");
+      
+      }
+     
+     
+      
+   }
+ }
