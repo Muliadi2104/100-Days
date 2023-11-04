126a4dee4c20969b349d031dc76faff04bd90b54
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-04 20:47:24 +0800

    Create Belajar coding day 27.java

---
+++ Belajar coding day 27.java
@@ -0,0 +1,18 @@
+package day27cetakdata;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Data yang mau na cetak
+        String nama ="Muliadi";
+        int umur    =19;
+        double tinggi =17.2;
+        
+        // Mencetak data
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + "tahun");
+        System.out.println("Tinggi: " + tinggi + "cm");
+    }
+    
+}
