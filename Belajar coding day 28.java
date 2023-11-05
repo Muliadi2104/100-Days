0f0129f03b4d844f0ac91e8167336b09a1b19890
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-05 22:52:02 +0800

    Create Belajar coding day 28.java

---
+++ Belajar coding day 28.java
@@ -0,0 +1,17 @@
+public class Biodata {
+    public static void main(String[] args) {
+        // Membuat variabel untuk menyimpan biodata
+        String nama = "Natan";
+        int umur = 19;
+        String alamat = "Polman";
+
+        // Mencetak biodata
+        System.out.println("Biodata:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Alamat: " + alamat);
+        
+        // Menambahkan komentar
+        System.out.println("Komentar: Andi adalah mahasiswa yang rajin dan bertanggung jawab.");
+    }
+}
