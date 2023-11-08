a4388ebae611092623de6dc42d1287624005ac94
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-08 23:59:04 +0800

    Create Belajar coding day 31.java

---
+++ Belajar coding day 31.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Dcoder!");
+    Scanner masuk = new Scanner (System.in);
+    int pilihan;
+    System.out.println ("_____Menu Makanan_____");
+    System.out. println ("1.bakso\n2.Mie ayam\n3.Nasi goreng");
+    System.out.println ("pilihan");
+    
+    pilihan = masuk.nextInt ();
+    
+    
+    
+    
+    
+    
+    
+    
+   }
+ }
