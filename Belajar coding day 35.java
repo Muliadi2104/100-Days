0059ddb2d1e333b0b9fce1358f4be17aa844b159
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-12 23:00:56 +0800

    Create Belajar coding day 35.java

---
+++ Belajar coding day 35.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int umur = 20;
+    if (umur <= 25){
+            System.out.println("masih mudah");
+    } else {
+        System.out.println("sudah tua");
+    }
+        
+        
+        
+    
+   }
+ }
