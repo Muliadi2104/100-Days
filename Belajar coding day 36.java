a8cd553571e0780698e8936a568d2321bbf51f7a
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-13 23:13:59 +0800

    Create Belajar coding day 36 percabangan switch.java

---
+++ Belajar coding day 36.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     // belajar menggunakan percabangan switch
+    int a = 20;
+    // nama variabel
+        switch (a){
+    // nilai kondisi switch di isi dengan variabel a
+            case 10: // kondisi
+            System.out.println ("halo");
+            //jika memenuhi kondisi ini yang di panggil 
+            break; // penghenti jika kondisi sudah terpenuhi 
+            case 20: // kondisi 
+            System.out.println("hayy");
+            //jika memenuhi kondisi ini yang di panggil 
+            break;
+        }
+   }
+ }
