31d60477803d083140645157586e2ee486ad68c1
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-15 21:58:35 +0800

    Belajar coding day 37 percabangan if else .java

---
+++ Belajar coding day 37 percabangan if else .java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+  
+
+    Scanner natan = new Scanner (System.in);
+    System.out.print("Masukkan nilai A = ");
+    int nilaiA = natan.nextInt();
+    
+    if (nilaiA >= 6){
+        System.out.println(nilaiA +" >="+ 6);
+    }
+    else if (nilaiA <= 6){
+        System.out.println(nilaiA + " <= "+ 6);
+    
+    
+}
+  }
+ }
