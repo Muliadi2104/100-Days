7334bc987fdc17110ecedaefd90497971965f0da
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-16 21:31:43 +0800

    Create Belajar coding day 39 perulangan for.java

---
+++ Belajar coding day 39 perulangan for.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Dcoder
+ {
+   public static void main(String args[])  { 
+     for (int a = 1; a <= 5; a++){
+       System.out.println ("Saya suka bermain voli");
+    }
+   }
+ }
