a771e99b4869eeff47a1ebf1fc2ad38f8fdc8105
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-23 20:27:18 +0800

    Create Belajar coding day 46.java

---
+++ Belajar coding day 46.java
@@ -0,0 +1,29 @@
+package natan;
+public class natan {
+    public static void main(String[] args) {
+        
+        int x = 9;
+        int y = 3;
+        
+        //Operator penjumlahan
+        int sum1 = x + y;
+        System.out.println("Penjumlahan: " + sum1);
+        
+        // Operator pengurangan
+        int sum2= x - y;
+        System.out.println("Pengurangan: " + sum2);
+        
+        // Operator perkalian
+        int sum3= x * y;
+        System.out.println("Perkalian: " + sum3);
+        
+        // Operator pembagian
+        int sum4 = x / y;
+        System.out.println("Pembagian: " + sum4);
+        
+        // Operator sisa bagi
+        int sum5 = x % y;
+        System.out.println("Sisa bagi: " + sum5);
+
+    }
+}
