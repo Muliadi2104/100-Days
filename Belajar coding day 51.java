b40084e6b4acfe2afd6e8dc92b3fdec099f98834
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-28 23:23:27 +0800

    Create Belajar coding day 51.java

---
+++ Belajar coding day 51.java
@@ -0,0 +1,15 @@
+package nop;
+
+public class mno {
+    
+    public static void main(String[] args) {
+        String nama [] = {"Andi","anwar","angga","arhan","amran"};
+        System.out.println(nama [0]);
+        System.out.println(nama [1]);
+        System.out.println(nama [2]);
+        System.out.println(nama [3]);
+        System.out.println(nama [4]);
+        
+    }
+    
+}
