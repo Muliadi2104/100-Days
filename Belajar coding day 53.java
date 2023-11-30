f1d11a4a4e6ef658af93c4313d4cd6ecdae6ff9f
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-11-30 20:14:30 +0800

    Create Belajar coding day 53.java

---
+++ Belajar coding day 53.java
@@ -0,0 +1,17 @@
+package opq;
+
+public class nop {
+    
+    public static void main(String[] args) {
+        
+        
+        int [] nilai = {70,90,86,95,78,200};
+        int jumlah = 0;
+        for(int a = 0; a < nilai.length; a++){
+            jumlah += nilai [a];
+        }
+        System.out.println(jumlah);
+    }
+    
+    }
+    
