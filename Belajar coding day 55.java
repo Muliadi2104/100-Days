1ffe5ae662c3029ca8c93c77fcc15158897409cf
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-02 21:36:07 +0800

    Create Belajar coding day 55.java

---
+++ Belajar coding day 55.java
@@ -0,0 +1,18 @@
+package stu;
+
+public class rst {
+    
+    public static void main(String[] args) {
+        
+        int[] nama = {45,56,77,65,98,76,85,90};
+        for (int i = 0; i < nama.length; i++) {
+            if (nama [i] >= 70){
+                System.out.println(nama [i]);
+            }
+            
+        }
+        
+    }
+    
+}
+    
