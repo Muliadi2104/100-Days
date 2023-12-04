af18887d0ed5122a9c5662c40d293b2553dc39cf
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-04 23:11:07 +0800

    Create Belajar coding day 57.java

---
+++ Belajar coding day 57.java
@@ -0,0 +1,17 @@
+package vwx;
+
+public class uvw {
+    
+    public static void main(String[] args) {
+        int[] nilaiMahasiwa = {70,90,86,95,78};
+        
+        int totalNilai = 0;
+        for (int nilai : nilaiMahasiwa) {
+            totalNilai += nilai;
+            
+        }
+        System.out.println("Total Nilai Mahsiswa: " + totalNilai);
+    }
+    
+
+    }
