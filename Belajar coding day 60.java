45a0698340e0deae9b941956d378c01479590dd8
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-07 21:23:05 +0800

    Create Belajar coding day 60.java

---
+++ Belajar coding day 60.java
@@ -0,0 +1,19 @@
+package wus;
+
+public class xwu {
+    
+    public static void main(String[] args) {
+        int a = 12;
+        if(a>=1 && a<=9){
+            System.out.println("satuan");
+        }if(a>=10 && a<=99){
+            System.out.println("puluhan");
+        }if(a>=100 && a<=999){
+            System.out.println("ratusan");
+        }
+    }
+    
+
+        
+    }
+    
