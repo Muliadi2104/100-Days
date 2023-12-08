fd6f26aaa68cd37a4828666324ef53c4fd69ea64
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-08 21:39:59 +0800

    Create Belajar coding day 61.java

---
+++ Belajar coding day 61.java
@@ -0,0 +1,24 @@
+package rahasia ;
+import java.util.Scanner;
+
+public class angka {
+    
+    public static void main(String[] args) {
+        
+        int kodeRahasia = 29;
+        while(true){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Masukkan kode rahasia : ");
+            int kode = input.nextInt();
+            if(kode == kodeRahasia){
+                System.out.println("Kode Benar");
+                break;
+            }else if(kode > kodeRahasia){
+                System.out.println("Kode yang anda masukkan terlalu besar\n");
+            }else if(kode < kodeRahasia){
+                System.out.println("Kode yang anda masukkan terlalu kecil\n");
+            }
+        }
+    }
+    
+}
