b632496117530a54e427978d08f53cbc65cd3579
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-09 22:14:29 +0800

    Create Belajar coding day 62.java

---
+++ Belajar coding day 62.java
@@ -0,0 +1,28 @@
+package mulya;
+
+public class andi {
+    
+    public static void main(String[] args) {
+        double total = 200000;
+        System.out.println("Belanja = Rp. "+total);
+        if (total >= 20000){
+            System.out.println("Anda mendapatkan diskon = 15%");
+            double diskon = 15 * total / 100;
+            total -= diskon;
+            
+          } else if ( total >= 15000){
+            System.out.println("Anda mendapatkan diskon = 10%");
+            double diskon = 10 * total / 100;
+            total -= diskon;
+        
+    }
+    else{
+        System.out.println ("Anda tidak mendapatkan diskon");
+        
+    }
+        System.out.println ("Total pembayaran anda = Rp. "+ total);
+    
+    
+    } 
+}
+        
