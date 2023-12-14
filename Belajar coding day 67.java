9d18bac53aef42ec1db03159c54d3d133a267710
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-14 20:48:59 +0800

    Create Belajar coding day 67.java

---
+++ Belajar coding day 67.java
@@ -0,0 +1,14 @@
+package abc;
+
+public class bca {
+    
+    public static void main(String[] args) {
+        String[] namateman = {"Uci","Sinta","Sari","Indar","Cici"};
+        
+        for(String nama : namateman){
+        System.out.println(nama);
+            
+        }
+    }
+    
+}
