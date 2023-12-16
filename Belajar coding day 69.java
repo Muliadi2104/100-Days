6b759b539f677fea4248e0f8148bffcc6c61c53a
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-16 22:10:16 +0800

    Create Belajar coding day 69.java

---
+++ Belajar coding day 69.java
@@ -0,0 +1,20 @@
+package nesia;
+
+public class indo {
+    
+    public static void main(String[] args) {
+        int a = 15;
+        System.out.println("ini adalah default");
+        if (a == 6){
+         System.out.println("benar");
+            
+        }else if(a == 15){
+            System.out.println("benar");
+        
+        }else{
+            System.out.println("salah");
+        }
+        
+    }
+    
+}
