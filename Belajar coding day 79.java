d3435aeb4d29a91d249bf4a496f96826c4b8c3fd
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-26 23:00:21 +0800

    Create Belajar coding day 79.java

---
+++ Belajar coding day 79.java
@@ -0,0 +1,15 @@
+package ppp;
+
+public class ddd {
+    
+    public static void main(String[] args) {
+        char huruf1 = 'A';
+        char huruf2 = 'B';
+        char huruf3 = 'C';
+        
+        System.out.println (huruf1);
+        System.out.println (huruf2);
+        System.out.println (huruf3);
+    }
+    
+}
