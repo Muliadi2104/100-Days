1dd2bbffa52bea6fcc76c6427657eab072a9bc6d
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-28 21:48:22 +0800

    Create Belajar coding day 81.java

---
+++ Belajar coding day 81.java
@@ -0,0 +1,14 @@
+package ajax;
+
+public class ava {
+    
+    public static void main(String[] args) {
+        int [] array= {60,90,50,95,78,200};
+        int jumlah = 0;
+        for(int a = 0; a < array.length; a++){
+            jumlah += array [a];
+        }
+        System.out.println(jumlah);
+    }
+    
+}
