6dc971063bd3bd4d3ffc509bc9d1727dd7135535
Muliadi2104 <147215779+Muliadi2104@users.noreply.github.com>
2023-12-30 22:38:22 +0800

    Create Belajar coding day 83.java

---
+++ Belajar coding day 83.java
@@ -0,0 +1,20 @@
+package dim;
+
+public class anda {
+    
+    public static void main(String[] args) {
+        String nama = "mulya";
+        String namA = "adi";
+        String ip;
+        
+        ip = nama;
+        nama = namA;
+        namA = ip;
+        
+        System.out.println(nama);
+        System.out.println (namA);
+        
+        
+    }
+    
+}
